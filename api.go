47b64d43104a0ce97108f95516417df521c18642
Siqi Wang <siqi@polyhedra.network>
2024-02-09 22:53:22 +0800

    move circuit ir print to correct place

--- api.go
+++ api.go
@@ -1,18 +1,12 @@
 package gkr
 
 import (
-	"fmt"
 	"math/big"
-	"strconv"
-	"strings"
 
 	"github.com/Zklib/gkr-compiler/builder"
-	"github.com/Zklib/gkr-compiler/expr"
 	"github.com/Zklib/gkr-compiler/ir"
 	"github.com/Zklib/gkr-compiler/layered"
 	"github.com/Zklib/gkr-compiler/layering"
-	"github.com/consensys/gnark/constraint"
-	"github.com/consensys/gnark/constraint/solver"
 	"github.com/consensys/gnark/frontend"
 )
 
@@ -64,65 +58,14 @@ func Compile(field *big.Int, circuit frontend.Circuit, pad2n bool, opts ...front
 	return &res, nil
 }
 
+func (c *CompileResult) GetCircuitIr() *ir.RootCircuit {
+	return c.rc
+}
+
 func (c *CompileResult) GetLayeredCircuit() *layered.RootCircuit {
 	return c.compiled
 }
 
 func (c *CompileResult) GetWitness(assignment frontend.Circuit) []*big.Int {
 	return c.rc.SolveInput(assignment, c.inputSolver)
-}
-
-func PrintCircuit(ci *ir.Circuit, field constraint.R1CS) {
-	varToStr := func(e expr.Expression) string {
-		s := make([]string, len(e))
-		for i, term := range e {
-			coeff := field.ToBigInt(term.Coeff).String()
-			if term.VID0 == 0 {
-				s[i] = coeff
-			} else if term.VID1 == 0 {
-				s[i] = "v" + strconv.Itoa(term.VID0) + "*" + coeff
-			} else {
-				s[i] = "v" + strconv.Itoa(term.VID0) + "*v" + strconv.Itoa(term.VID1) + "*" + coeff
-			}
-		}
-		return strings.Join(s, "+")
-	}
-
-	for _, insn := range ci.Instructions {
-		if insn.Type == ir.IInternalVariable {
-			fmt.Printf("v%d = %s\n", insn.OutputIds[0], varToStr(insn.Inputs[0]))
-		} else if insn.Type == ir.IHint {
-			strs := make([]string, len(insn.Inputs))
-			for i, x := range insn.Inputs {
-				strs[i] = varToStr(x)
-			}
-			fmt.Printf("v%d", insn.OutputIds[0])
-			for i := 1; i < len(insn.OutputIds); i++ {
-				fmt.Printf(",v%d", insn.OutputIds[i])
-			}
-			fmt.Printf(" = %s(%s)\n", solver.GetHintName(insn.HintFunc), strings.Join(strs, ","))
-		} else if insn.Type == ir.ISubCircuit {
-			strs := make([]string, len(insn.Inputs))
-			for i, x := range insn.Inputs {
-				strs[i] = varToStr(x)
-			}
-			fmt.Printf("v%d", insn.OutputIds[0])
-			for i := 1; i < len(insn.OutputIds); i++ {
-				fmt.Printf(",v%d", insn.OutputIds[i])
-			}
-			fmt.Printf(" = sub%d(%s)\n", insn.SubCircuitId, strings.Join(strs, ","))
-		}
-	}
-
-	for i, e := range ci.Output {
-		fmt.Printf("out%d = %s\n", i, varToStr(e))
-	}
-}
-
-func (c *CompileResult) Print() {
-	for k, v := range c.rc.Circuits {
-		fmt.Printf("Circuit %d nbIn=%d nbOut=%d =================\n", k, v.NbExternalInput, len(v.Output))
-		PrintCircuit(v, c.rc.Field)
-		fmt.Println()
-	}
 }
