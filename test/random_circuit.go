daeb269eb90809bfdcef90b305b35e8a3a888b4e
Siqi Wang <siqi@polyhedra.network>
2024-02-12 03:15:17 +0800

    support more types of sub circuit functions by reflect

--- test/random_circuit.go
+++ test/random_circuit.go
@@ -153,7 +153,7 @@ func (rcg *randomCircuitGenerator) randomCircuit(api frontend.API, input []front
 					subIn[i] = vars[p[i]]
 				}
 				//subOut := rcg.randomCircuit(api, subIn)
-				subOut := api.(gkr.API).MemorizedCall(rcg.randomCircuit, subIn)
+				subOut := api.(gkr.API).MemorizedSimpleCall(rcg.randomCircuit, subIn)
 				vars = append(vars, subOut...)
 			}
 		}
