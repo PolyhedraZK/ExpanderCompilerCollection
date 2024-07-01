5d6a416b8510e5a41f5293cd3c8bd3ea52b3d9a8
hczphn <hczphn@polyhedra.network>
2024-06-30 23:06:12 -0500

    new plookup API

--- std/plookup/sortPair.go
+++ std/plookup/sortPair.go
@@ -1,9 +1,7 @@
 package plookup
 
 import (
-	"fmt"
 	"math/big"
-	"sort"
 )
 
 type KeyValuePair struct {
@@ -23,38 +21,4 @@ func (vip KeyValuePairs) Swap(i, j int) {
 
 func (vip KeyValuePairs) Less(i, j int) bool {
 	return vip[i].Key.Cmp(vip[j].Key) < 0
-}
-
-func Testmain() {
-	arr := []*big.Int{
-		big.NewInt(64),
-		big.NewInt(34),
-		big.NewInt(25),
-		big.NewInt(12),
-		big.NewInt(22),
-		big.NewInt(11),
-		big.NewInt(90),
-	}
-
-	valueIndexPairs := make(KeyValuePairs, len(arr))
-	for i, v := range arr {
-		valueIndexPairs[i] = KeyValuePair{Key: v, Value: big.NewInt(int64(i))}
-	}
-
-	sort.Sort(valueIndexPairs)
-
-	sortedIndexes := make([]*big.Int, len(arr))
-	for i, pair := range valueIndexPairs {
-		sortedIndexes[i] = pair.Value
-	}
-
-	fmt.Println("Sorted values and their original indexes:")
-	for _, pair := range valueIndexPairs {
-		fmt.Printf("Value: %s, Original Index: %s\n", pair.Key.String(), pair.Value.String())
-	}
-
-	fmt.Println("Sorted indexes:")
-	for _, index := range sortedIndexes {
-		fmt.Println(index.String())
-	}
 }
--- std/plookup/sortPair_test.go
+++ std/plookup/sortPair_test.go
@@ -1,7 +1,42 @@
 package plookup
 
-import "testing"
+import (
+	"fmt"
+	"math/big"
+	"sort"
+	"testing"
+)
 
-func TestTheMain(t *testing.T) {
-	Testmain()
+func TestSort(t *testing.T) {
+	arr := []*big.Int{
+		big.NewInt(64),
+		big.NewInt(34),
+		big.NewInt(25),
+		big.NewInt(12),
+		big.NewInt(22),
+		big.NewInt(11),
+		big.NewInt(90),
+	}
+
+	valueIndexPairs := make(KeyValuePairs, len(arr))
+	for i, v := range arr {
+		valueIndexPairs[i] = KeyValuePair{Key: v, Value: big.NewInt(int64(i))}
+	}
+
+	sort.Sort(valueIndexPairs)
+
+	sortedIndexes := make([]*big.Int, len(arr))
+	for i, pair := range valueIndexPairs {
+		sortedIndexes[i] = pair.Value
+	}
+
+	fmt.Println("Sorted values and their original indexes:")
+	for _, pair := range valueIndexPairs {
+		fmt.Printf("Value: %s, Original Index: %s\n", pair.Key.String(), pair.Value.String())
+	}
+
+	fmt.Println("Sorted indexes:")
+	for _, index := range sortedIndexes {
+		fmt.Println(index.String())
+	}
 }
