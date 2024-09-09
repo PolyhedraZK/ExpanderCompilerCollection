37897a1dc9e49925bba45a46c01308e1a66c490f
siq1 <siqi@polyhedra.network>
2024-09-09 09:50:41 +0700

    update wrapper

--- api.go
+++ api.go
@@ -6,3 +6,5 @@ type API = ecgo.API
 type CompileResult = ecgo.CompileResult
 
 var Compile = ecgo.Compile
+var DeserializeLayeredCircuit = ecgo.DeserializeLayeredCircuit
+var DeserializeInputSolver = ecgo.DeserializeInputSolver
