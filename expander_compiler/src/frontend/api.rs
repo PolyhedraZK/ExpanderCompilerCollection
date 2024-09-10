c48ca711d0617611f73a2815fc1fed0c7ed5a818
siq1 <siqi@polyhedra.network>
2024-09-11 02:35:17 +0700

    add unconstrained ops in frontend

--- expander_compiler/src/frontend/api.rs
+++ expander_compiler/src/frontend/api.rs
@@ -2,45 +2,32 @@ use crate::circuit::config::Config;
 
 use super::builder::{ToVariableOrValue, Variable};
 
+// write a macro rules to generate binary op definition
+macro_rules! binary_op {
+    ($name:ident) => {
+        fn $name(
+            &mut self,
+            x: impl ToVariableOrValue<C::CircuitField>,
+            y: impl ToVariableOrValue<C::CircuitField>,
+        ) -> Variable;
+    };
+}
+
 pub trait BasicAPI<C: Config> {
-    fn add(
-        &mut self,
-        x: impl ToVariableOrValue<C::CircuitField>,
-        y: impl ToVariableOrValue<C::CircuitField>,
-    ) -> Variable;
-    fn sub(
-        &mut self,
-        x: impl ToVariableOrValue<C::CircuitField>,
-        y: impl ToVariableOrValue<C::CircuitField>,
-    ) -> Variable;
-    fn neg(&mut self, x: impl ToVariableOrValue<C::CircuitField>) -> Variable;
-    fn mul(
-        &mut self,
-        x: impl ToVariableOrValue<C::CircuitField>,
-        y: impl ToVariableOrValue<C::CircuitField>,
-    ) -> Variable;
+    binary_op!(add);
+    binary_op!(sub);
+    binary_op!(mul);
+    binary_op!(xor);
+    binary_op!(or);
+    binary_op!(and);
     fn div(
         &mut self,
         x: impl ToVariableOrValue<C::CircuitField>,
         y: impl ToVariableOrValue<C::CircuitField>,
         checked: bool,
     ) -> Variable;
+    fn neg(&mut self, x: impl ToVariableOrValue<C::CircuitField>) -> Variable;
     fn inverse(&mut self, x: impl ToVariableOrValue<C::CircuitField>) -> Variable;
-    fn xor(
-        &mut self,
-        x: impl ToVariableOrValue<C::CircuitField>,
-        y: impl ToVariableOrValue<C::CircuitField>,
-    ) -> Variable;
-    fn or(
-        &mut self,
-        x: impl ToVariableOrValue<C::CircuitField>,
-        y: impl ToVariableOrValue<C::CircuitField>,
-    ) -> Variable;
-    fn and(
-        &mut self,
-        x: impl ToVariableOrValue<C::CircuitField>,
-        y: impl ToVariableOrValue<C::CircuitField>,
-    ) -> Variable;
     fn is_zero(&mut self, x: impl ToVariableOrValue<C::CircuitField>) -> Variable;
     fn assert_is_zero(&mut self, x: impl ToVariableOrValue<C::CircuitField>);
     fn assert_is_non_zero(&mut self, x: impl ToVariableOrValue<C::CircuitField>);
@@ -55,4 +42,27 @@ pub trait BasicAPI<C: Config> {
         x: impl ToVariableOrValue<C::CircuitField>,
         y: impl ToVariableOrValue<C::CircuitField>,
     );
+}
+
+pub trait UnconstrainedAPI<C: Config> {
+    fn unconstrained_identity(&mut self, x: impl ToVariableOrValue<C::CircuitField>) -> Variable;
+    binary_op!(unconstrained_add);
+    binary_op!(unconstrained_mul);
+    binary_op!(unconstrained_div);
+    binary_op!(unconstrained_pow);
+    binary_op!(unconstrained_int_div);
+    binary_op!(unconstrained_mod);
+    binary_op!(unconstrained_shift_l);
+    binary_op!(unconstrained_shift_r);
+    binary_op!(unconstrained_lesser_eq);
+    binary_op!(unconstrained_greater_eq);
+    binary_op!(unconstrained_lesser);
+    binary_op!(unconstrained_greater);
+    binary_op!(unconstrained_eq);
+    binary_op!(unconstrained_not_eq);
+    binary_op!(unconstrained_bool_or);
+    binary_op!(unconstrained_bool_and);
+    binary_op!(unconstrained_bit_or);
+    binary_op!(unconstrained_bit_and);
+    binary_op!(unconstrained_bit_xor);
 }
