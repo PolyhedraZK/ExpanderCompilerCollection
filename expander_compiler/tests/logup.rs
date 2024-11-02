f69fd7190488fa889766b68cc7c67ca981a4732f
Zhiyong Fang <zhiyong.fang.1997@gmail.com>
2024-11-02 09:59:20 -0500

    refactor

--- expander_compiler/tests/logup.rs
+++ expander_compiler/tests/logup.rs
@@ -1,112 +1,200 @@
-use std::{default, ops::Add};
-
 use arith::Field;
 use expander_compiler::frontend::*;
 use extra::Serde;
-use rand::{random, thread_rng};
+use rand::{thread_rng, Rng};
 
-const N_TABLE_ROWS: uint = 16;
-const N_COLUMNS: uint = 2;
-const N_QUERIES: uint = 32;
+const KEY_LEN: usize = 3;
+const N_TABLE_ROWS: usize = 17;
+const N_COLUMNS: usize = 5;
+const N_QUERIES: usize = 33;
 
 declare_circuit!(Circuit {
-    table: [[Variable; N_COLUMNS]; N_TABLE_ROWS],
-    
-    // this will not be used for now, but later if we switch to hint, we'll need these ids
-    query_ids: [Variable; N_QUERIES], 
-    query_results: [Variable; N_QUERIES],
+    table_keys: [[Variable; KEY_LEN]; N_TABLE_ROWS],
+    table_values: [[Variable; N_COLUMNS]; N_TABLE_ROWS],
 
-    // It would be great if we can compute this using hint, but acceptable as private input
-    query_count: [Variable; N_TABLE_ROWS], 
+    query_keys: [[Variable; KEY_LEN]; N_QUERIES],
+    query_results: [[Variable; N_COLUMNS]; N_QUERIES],
+
+    // counting the number of occurences for each row of the table
+    query_count: [Variable; N_TABLE_ROWS],
 });
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Rational {
     numerator: Variable,
     denominator: Variable,
 }
 
-fn add_rational(builder: &mut API<M31Config>, v1: &Rational, v2: &Rational) -> Rational {
-    return Rational {
-        numerator: builder.add(builder.mul(v1.numerator, v2.denominator), builder.mul(v1.denominator, v2.numerator)),
+fn add_rational<C: Config>(builder: &mut API<C>, v1: &Rational, v2: &Rational) -> Rational {
+    let p1 = builder.mul(v1.numerator, v2.denominator);
+    let p2 = builder.mul(v1.denominator, v2.numerator);
+
+    Rational {
+        numerator: builder.add(p1, p2),
         denominator: builder.mul(v1.denominator, v2.denominator),
     }
 }
 
-fn sum_rational_vec(builder: &mut API<M31Config>, vs: &[Rational]) -> Rational {
-    assert!(vs.len().is_power_of_two());
-    let mut vs = vs.clone();
-    let mut cur_output_len = vs.len() >> 1;
-    while cur_output_len > 0 {
-        for i in 0..cur_output_len {
-            vs[i] = add_rational(builder, &vs[i], &vs[i << 1]);
-        }
-        cur_output_len >>= 1;
-    }
-    return vs[0];
+fn assert_eq_rational<C: Config>(builder: &mut API<C>, v1: &Rational, v2: &Rational) {
+    let p1 = builder.mul(v1.numerator, v2.denominator);
+    let p2 = builder.mul(v1.denominator, v2.numerator);
+    builder.assert_is_equal(p1, p2);
 }
 
-fn combine_columns(builder: &mut API<M31Config>, vec_2d: &Vec<Vec<Variable>>, randomness: &[Variable]) -> Vec<Variable> {
+fn sum_rational_vec<C: Config>(builder: &mut API<C>, vs: &[Rational]) -> Rational {
+    if vs.is_empty() {
+        return Rational {
+            numerator: builder.constant(0),
+            denominator: builder.constant(1),
+        }
+    }
+    
+    // Basic version:
+    // let mut sum = Rational {
+    //     numerator: builder.constant(0),
+    //     denominator: builder.constant(1),
+    // };
+    // for i in 0..vs.len() {
+    //     sum = add_rational(builder, &sum, &vs[i]);
+    // }
+    // sum
+
+    // Fewer-layers version:
+    let mut vvs = vs.to_owned();
+    let mut n_values_to_sum = vvs.len();
+    while n_values_to_sum > 1 {
+        let half_size_floor = n_values_to_sum / 2;
+        for i in 0..half_size_floor {
+            vvs[i] = add_rational(builder, &vvs[i], &vvs[i + half_size_floor])
+        }
+
+        if n_values_to_sum & 1 != 0 {
+            vvs[half_size_floor] = vvs[n_values_to_sum - 1];
+        }
+
+        n_values_to_sum = (n_values_to_sum + 1) / 2;
+    }
+    vvs[0]
+}
+
+// TODO: Add poly randomness
+fn get_column_randomness<C: Config>(builder: &mut API<C>, n_columns: usize) -> Vec<Variable> {
+    let mut randomness = vec![];
+    randomness[0] = builder.constant(1);
+    for _ in 1..n_columns {
+        randomness.push(builder.get_random_value());
+    }
+    randomness
+}
+
+fn combine_columns<C: Config>(
+    builder: &mut API<C>,
+    vec_2d: &Vec<Vec<Variable>>,
+    randomness: &[Variable],
+) -> Vec<Variable> {
     if vec_2d.is_empty() {
         return vec![];
     }
 
-    let column_size = len(vec_2d[0]);
+    let column_size = vec_2d[0].len();
     assert!(randomness.len() == column_size);
-
-    vec_2d.iter()
-          .map(|column| column.iter()
-            .zip(randomness).fold(0, |(acc, (v, r))| builder.add(acc, builder.mul(v, r))))
-          .collect()
+    vec_2d
+        .iter()
+        .map(|row| {
+            row.iter()
+                .zip(randomness)
+                .fold(builder.constant(0), |acc, (v, r)| {
+                    let prod = builder.mul(v, r);
+                    builder.add(acc, prod)
+                })
+        })
+        .collect()
 }
 
-fn logup_poly_val(builder: &mut API<M31Config>, vals: &[Variable], counts: &[Variable], x: &Variable) -> Rational {
+fn logup_poly_val<C: Config>(
+    builder: &mut API<C>,
+    vals: &[Variable],
+    counts: &[Variable],
+    x: &Variable,
+) -> Rational {
     let poly_terms = vals
-    .iter().zip(counts)
-    .map(|(v, c)| 
-        Rational {
-            numerator: c,
+        .iter()
+        .zip(counts)
+        .map(|(v, c)| Rational {
+            numerator: *c,
             denominator: builder.sub(x, v),
         })
-    .collect::<Vec<Rational>>();
+        .collect::<Vec<Rational>>();
     sum_rational_vec(builder, &poly_terms)
 }
 
-impl Define<M31Config> for Circuit<Variable> {
-    fn define(&self, builder: &mut API<M31Config>) {
-        let table_combined = combine_columns(builder, self.table, &self.randomness);
-        let v_left = logup_poly_val(builder, &table_combined, &self.query_count, &self.challenge);        
-        
-        let query_combined = combine_columns(builder, self.query_results, &self.randomness);
-        let v_right = logup_poly_val(builder, &query_combined, &vec![1; query_combined.len()], &self.challenge);
-        builder.assert_is_equal(
-            builder.mul(v_left.numerator, v_right.denominator),
-            builder.mul(v_right.numerator, v_left.denominator), 
+impl<C: Config> Define<C> for Circuit<Variable> {
+    fn define(&self, builder: &mut API<C>) {
+        let alpha = builder.get_random_value();
+        let randomness = get_column_randomness(builder, KEY_LEN + N_COLUMNS);
+
+        let table_combined = combine_columns(
+            builder,
+            &self
+                .table_keys
+                .iter()
+                .zip(self.table_values)
+                .map(|(row_key, row_value)| [row_key.to_vec(), row_value.to_vec()].concat())
+                .collect(),
+            &randomness,
         );
+        let v_table = logup_poly_val(builder, &table_combined, &self.query_count, &alpha);
+
+        let query_combined = combine_columns(
+            builder,
+            &self
+                .query_keys
+                .iter()
+                .zip(self.query_results)
+                .map(|(row_key, row_value)| [row_key.to_vec(), row_value.to_vec()].concat())
+                .collect(),
+            &randomness,
+        );
+        let one = builder.constant(1);
+        let v_query = logup_poly_val(
+            builder,
+            &query_combined,
+            &vec![one; query_combined.len()],
+            &alpha,
+        );
+
+        assert_eq_rational(builder, &v_table, &v_query);
     }
 }
 
 #[inline]
-fn gen_assignment() -> Circuit<M31> {
-    let mut circuit = Circuit<M31>::default();
+fn gen_assignment<C: Config>() -> Circuit<C::CircuitField> {
+    let mut circuit = Circuit::<C::CircuitField>::default();
     let mut rng = thread_rng();
     for i in 0..N_TABLE_ROWS {
+        for j in 0..KEY_LEN {
+            circuit.table_keys[i][j] = C::CircuitField::random_unsafe(&mut rng);
+        }
+
         for j in 0..N_COLUMNS {
-            circuit.table[i][j] = M31::random_unsafe(rng);
+            circuit.table_values[i][j] = C::CircuitField::random_unsafe(&mut rng);
         }
     }
 
+    circuit.query_count = [C::CircuitField::ZERO; N_TABLE_ROWS];
     for i in 0..N_QUERIES {
-        circuit.query_ids
+        let query_id: usize = rng.gen::<usize>() % N_TABLE_ROWS;
+        circuit.query_count[query_id] += C::CircuitField::ONE;
+        circuit.query_keys[i] = circuit.table_keys[query_id];
+        circuit.query_results[i] = circuit.table_values[query_id];
     }
 
     circuit
 }
 
-#[test]
-fn logup() {
-    let compile_result = compile(&Circuit::default()).unwrap();
-    let assignment = gen_assignment();
+fn logup_test_helper<C: Config>() {
+    let compile_result: CompileResult<C> = compile(&Circuit::default()).unwrap();
+    let assignment = gen_assignment::<C>();
     let witness = compile_result
         .witness_solver
         .solve_witness(&assignment)
@@ -131,4 +219,12 @@ fn logup() {
         .witness_solver
         .serialize_into(writer)
         .unwrap();
+}
+
+
+#[test]
+fn logup_test() {
+    logup_test_helper::<GF2Config>();
+    logup_test_helper::<M31Config>();
+    logup_test_helper::<BN254Config>();
 }
