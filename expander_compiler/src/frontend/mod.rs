b10aa7f7f542ac699eee3016b53933bbf8a90fe5
siq1 <166227013+siq1@users.noreply.github.com>
2024-12-10 22:27:08 +0900

    Debugging Evalution (#49)
    * implement debug builder
    
    * fmt

--- expander_compiler/src/frontend/mod.rs
+++ expander_compiler/src/frontend/mod.rs
@@ -5,6 +5,7 @@ use crate::circuit::{ir, layered};
 mod api;
 mod builder;
 mod circuit;
+mod debug;
 mod variables;
 mod witness;
 
@@ -14,9 +15,9 @@ pub use crate::circuit::config::*;
 pub use crate::compile::CompileOptions;
 pub use crate::field::{Field, BN254, GF2, M31};
 pub use crate::utils::error::Error;
-pub use api::BasicAPI;
+pub use api::{BasicAPI, RootAPI};
 pub use builder::Variable;
-pub use circuit::Define;
+pub use circuit::{Define, GenericDefine};
 pub use witness::WitnessSolver;
 
 pub mod internal {
@@ -29,12 +30,44 @@ pub mod internal {
 }
 
 pub mod extra {
-    pub use super::api::UnconstrainedAPI;
+    pub use super::api::{DebugAPI, UnconstrainedAPI};
+    pub use super::debug::DebugBuilder;
     pub use crate::utils::serde::Serde;
+
+    use super::*;
+
+    pub fn debug_eval<
+        C: Config,
+        Cir: internal::DumpLoadTwoVariables<Variable> + GenericDefine<C> + Clone,
+        CA: internal::DumpLoadTwoVariables<C::CircuitField>,
+    >(
+        circuit: &Cir,
+        assignment: &CA,
+    ) {
+        let (num_inputs, num_public_inputs) = circuit.num_vars();
+        let (a_num_inputs, a_num_public_inputs) = assignment.num_vars();
+        assert_eq!(num_inputs, a_num_inputs);
+        assert_eq!(num_public_inputs, a_num_public_inputs);
+        let mut inputs = Vec::new();
+        let mut public_inputs = Vec::new();
+        assignment.dump_into(&mut inputs, &mut public_inputs);
+        let (mut root_builder, input_variables, public_input_variables) =
+            DebugBuilder::<C>::new(inputs, public_inputs);
+        let mut circuit = circuit.clone();
+        let mut vars_ptr = input_variables.as_slice();
+        let mut public_vars_ptr = public_input_variables.as_slice();
+        circuit.load_from(&mut vars_ptr, &mut public_vars_ptr);
+        circuit.define(&mut root_builder);
+    }
 }
 
 #[cfg(test)]
 mod tests;
+
+pub struct CompileResult<C: Config> {
+    pub witness_solver: WitnessSolver<C>,
+    pub layered_circuit: layered::Circuit<C>,
+}
 
 fn build<C: Config, Cir: internal::DumpLoadTwoVariables<Variable> + Define<C> + Clone>(
     circuit: &Cir,
@@ -50,11 +83,6 @@ fn build<C: Config, Cir: internal::DumpLoadTwoVariables<Variable> + Define<C> + 
     root_builder.build()
 }
 
-pub struct CompileResult<C: Config> {
-    pub witness_solver: WitnessSolver<C>,
-    pub layered_circuit: layered::Circuit<C>,
-}
-
 pub fn compile<C: Config, Cir: internal::DumpLoadTwoVariables<Variable> + Define<C> + Clone>(
     circuit: &Cir,
 ) -> Result<CompileResult<C>, Error> {
@@ -66,14 +94,31 @@ pub fn compile<C: Config, Cir: internal::DumpLoadTwoVariables<Variable> + Define
     })
 }
 
-pub fn compile_with_options<
+fn build_generic<
     C: Config,
-    Cir: internal::DumpLoadTwoVariables<Variable> + Define<C> + Clone,
+    Cir: internal::DumpLoadTwoVariables<Variable> + GenericDefine<C> + Clone,
+>(
+    circuit: &Cir,
+) -> ir::source::RootCircuit<C> {
+    let (num_inputs, num_public_inputs) = circuit.num_vars();
+    let (mut root_builder, input_variables, public_input_variables) =
+        RootBuilder::<C>::new(num_inputs, num_public_inputs);
+    let mut circuit = circuit.clone();
+    let mut vars_ptr = input_variables.as_slice();
+    let mut public_vars_ptr = public_input_variables.as_slice();
+    circuit.load_from(&mut vars_ptr, &mut public_vars_ptr);
+    circuit.define(&mut root_builder);
+    root_builder.build()
+}
+
+pub fn compile_generic<
+    C: Config,
+    Cir: internal::DumpLoadTwoVariables<Variable> + GenericDefine<C> + Clone,
 >(
     circuit: &Cir,
     options: CompileOptions,
 ) -> Result<CompileResult<C>, Error> {
-    let root = build(circuit);
+    let root = build_generic(circuit);
     let (irw, lc) = crate::compile::compile_with_options::<C>(&root, options)?;
     Ok(CompileResult {
         witness_solver: WitnessSolver { circuit: irw },
--- expander_compiler/tests/mul_fanout_limit.rs
+++ expander_compiler/tests/mul_fanout_limit.rs
@@ -6,8 +6,8 @@ declare_circuit!(Circuit {
     sum: Variable,
 });
 
-impl Define<M31Config> for Circuit<Variable> {
-    fn define(&self, builder: &mut API<M31Config>) {
+impl GenericDefine<M31Config> for Circuit<Variable> {
+    fn define<Builder: RootAPI<M31Config>>(&self, builder: &mut Builder) {
         let mut sum = builder.constant(0);
         for i in 0..16 {
             for j in 0..512 {
@@ -20,7 +20,7 @@ impl Define<M31Config> for Circuit<Variable> {
 }
 
 fn mul_fanout_limit(limit: usize) {
-    let compile_result = compile_with_options(
+    let compile_result = compile_generic(
         &Circuit::default(),
         CompileOptions::default().with_mul_fanout_limit(limit),
     )
