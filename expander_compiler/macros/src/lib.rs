8e2dd0798d24c5ac92f9436dd18243ab8df3c976
siq1 <siqi@polyhedra.network>
2024-12-19 02:34:16 +0800

    update

--- expander_compiler/macros/src/lib.rs
+++ expander_compiler/macros/src/lib.rs
@@ -46,65 +46,171 @@ fn get_variable_spec(ty: &Type) -> (bool, bool) {
     }
 }
 
+fn generate_array_access(vars: &[proc_macro2::Ident]) -> proc_macro2::TokenStream {
+    let mut access = quote! {};
+    for var in vars {
+        access = quote! { #access[#var] };
+    }
+    access
+}
+
 fn generate_unflatten_code(
-    name: &proc_macro2::TokenStream,
+    array_index: usize,
+    output_name: &proc_macro2::TokenStream,
     ty: &Type,
-    offset: &proc_macro2::TokenStream,
 ) -> proc_macro2::TokenStream {
-    match ty {
-        Type::Array(array) => {
-            let len = &array.len;
-            let inner_code = generate_unflatten_code(name, &array.elem, offset);
-            quote! {
-                {
-                    let mut temp = Vec::with_capacity(#len as usize);
-                    let mut local_offset = #offset;
-                    for _ in 0..#len {
-                        temp.push(#inner_code);
-                        local_offset += calculate_inner_len(&#array.elem);
-                    }
-                    temp
-                }
+    fn collect_dimensions(ty: &Type) -> Vec<&syn::Expr> {
+        let mut dims = Vec::new();
+        let mut curr_ty = ty;
+        while let Type::Array(array) = curr_ty {
+            dims.push(&array.len);
+            curr_ty = &array.elem;
+        }
+        dims
+    }
+
+    let dims = collect_dimensions(ty);
+    if dims.is_empty() {
+        panic!("Expected array type");
+    }
+
+    // 生成步长计算代码
+    let mut steps = Vec::with_capacity(dims.len());
+    let mut step = quote! { 1 };
+    for &dim in dims.iter().rev().skip(1) {
+        step = quote! { #dim * (#step) };
+        steps.push(step.clone());
+    }
+    steps.reverse();
+
+    // 生成循环变量
+    let loop_vars = (0..dims.len())
+        .map(|i| format_ident!("i_{}", i))
+        .collect::<Vec<_>>();
+
+    // 生成索引计算代码
+    let mut index_calc = quote! { 0 };
+    for (i, (step, var)) in steps.iter().zip(loop_vars.iter()).enumerate() {
+        if i == 0 {
+            index_calc = quote! { #var * (#step) };
+        } else {
+            index_calc = quote! { #index_calc + #var * (#step) };
+        }
+    }
+    if let Some(last_var) = loop_vars.last() {
+        index_calc = quote! { #index_calc + #last_var };
+    }
+
+    // 生成嵌套循环代码
+    let array_access = generate_array_access(&loop_vars[..loop_vars.len() - 1]);
+    let mut inner_code = quote! {
+        #output_name #array_access.push(inputs[#array_index][#index_calc].clone());
+    };
+
+    // 从内到外生成嵌套循环
+    for (i, (var, &dim)) in loop_vars.iter().zip(dims.iter()).enumerate().rev() {
+        let init_code = if i == loop_vars.len() - 1 {
+            quote! {}
+        } else if i == 0 {
+            quote! { #output_name.push(Vec::new()); }
+        } else {
+            let init_dims = &loop_vars[..i];
+            let array_access = generate_array_access(init_dims);
+            quote! { #output_name #array_access.push(Vec::new()); }
+        };
+
+        inner_code = quote! {
+            for #var in 0..#dim {
+                #init_code
+                #inner_code
             }
-        }
-        Type::Path(_) => {
-            quote! { #name[#offset].clone() }
-        }
-        _ => panic!("Unexpected type in unflatten"),
+        };
+    }
+
+    // 生成最终代码
+    quote! {
+        let mut #output_name: Vec<_> = Vec::new();
+        #inner_code
     }
 }
 
-fn generate_flatten_code(name: &proc_macro2::TokenStream, ty: &Type) -> proc_macro2::TokenStream {
-    match ty {
-        Type::Array(array) => {
-            let inner_code = generate_flatten_code(&quote! { inner }, &array.elem);
-            quote! {
-                {
-                    let mut flat = Vec::new();
-                    for inner in #name.iter() {
-                        flat.extend(#inner_code);
-                    }
-                    flat
-                }
-            }
+fn generate_flatten_code(
+    array_index: usize,
+    input_name: &proc_macro2::TokenStream,
+    ty: &Type,
+) -> proc_macro2::TokenStream {
+    fn collect_dimensions(ty: &Type) -> Vec<&syn::Expr> {
+        let mut dims = Vec::new();
+        let mut curr_ty = ty;
+        while let Type::Array(array) = curr_ty {
+            dims.push(&array.len);
+            curr_ty = &array.elem;
         }
-        Type::Path(_) => {
-            quote! { vec![#name.clone()] }
-        }
-        _ => panic!("Unexpected type in flatten"),
+        dims
     }
+
+    let dims = collect_dimensions(ty);
+    if dims.is_empty() {
+        panic!("Expected array type");
+    }
+
+    // 生成步长计算代码
+    let mut steps = Vec::with_capacity(dims.len());
+    let mut step = quote! { 1 };
+    for &dim in dims.iter().rev().skip(1) {
+        step = quote! { #dim * (#step) };
+        steps.push(step.clone());
+    }
+    steps.reverse();
+
+    // 生成循环变量
+    let loop_vars = (0..dims.len())
+        .map(|i| format_ident!("i_{}", i))
+        .collect::<Vec<_>>();
+
+    // 生成索引计算代码
+    let mut index_calc = quote! { 0 };
+    for (i, (step, var)) in steps.iter().zip(loop_vars.iter()).enumerate() {
+        if i == 0 {
+            index_calc = quote! { #var * (#step) };
+        } else {
+            index_calc = quote! { #index_calc + #var * (#step) };
+        }
+    }
+    if let Some(last_var) = loop_vars.last() {
+        index_calc = quote! { #index_calc + #last_var };
+    }
+
+    // 生成嵌套循环代码
+    let array_access = generate_array_access(&loop_vars);
+    let mut loop_code = quote! {
+        inputs[#array_index][#index_calc] = #input_name #array_access.clone();
+    };
+
+    for (var, &dim) in loop_vars.iter().zip(dims.iter()).rev() {
+        loop_code = quote! {
+            for #var in 0..#dim {
+                #loop_code
+            }
+        };
+    }
+
+    // 生成最终代码，直接使用inputs
+    quote! { #loop_code }
 }
 
 #[proc_macro_attribute]
 pub fn kernel(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // eprintln!("Input tokens: {:#?}", item);
     let input_fn = parse_macro_input!(item as ItemFn);
     let fn_name = &input_fn.sig.ident;
-    let user_fn_name = format_ident!("user_{}", fn_name);
+    let user_fn_name = format_ident!("{}", fn_name);
     let compile_fn_name = format_ident!("compile_{}", fn_name);
     let stmts = &input_fn.block.stmts;
 
     let mut specs = Vec::new();
     let mut arg_names = Vec::new();
+    let mut arg_mutability = Vec::new(); // 存储每个参数是否可变
     let mut unflatten_code = Vec::new();
     let mut flatten_code = Vec::new();
     let mut current_index = 0;
@@ -115,89 +221,98 @@ pub fn kernel(_attr: TokenStream, item: TokenStream) -> TokenStream {
         .first()
         .expect("Expected at least one argument (API)");
 
-    let user_fn_inputs: Vec<_> = input_fn
-        .sig
-        .inputs
-        .iter()
-        .skip(1)
-        .map(|arg| {
-            if let FnArg::Typed(PatType { pat, ty, .. }) = arg {
-                if let Type::Reference(ref_type) = &**ty {
-                    if let Type::Array(array) = &*ref_type.elem {
-                        let mut inner_ty = &*array.elem;
-                        while let Type::Array(arr) = inner_ty {
-                            inner_ty = &*arr.elem;
+    let user_fn_inputs: Vec<_> = input_fn.sig.inputs.iter().skip(1).map(|arg| {
+        if let FnArg::Typed(PatType { pat, ty, .. }) = arg {
+            if let Type::Reference(ref_type) = &**ty {
+                if let Type::Array(_) = *ref_type.elem {
+                    let vec_type = replace_array_with_vec(&ref_type.elem);
+                    let arg_name = quote! { #pat };
+
+                    // 获取最内层类型
+                    let mut inner_ty = &*ref_type.elem;
+                    while let Type::Array(arr) = inner_ty {
+                        inner_ty = &*arr.elem;
+                    }
+                    let (is_input, is_output) = get_variable_spec(inner_ty);
+                    let total_len = calculate_array_total_len(&ref_type.elem);
+
+                    specs.push(quote! {
+                        IOVecSpec {
+                            len: #total_len,
+                            is_input: #is_input,
+                            is_output: #is_output,
                         }
+                    });
 
-                        let (is_input, is_output) = get_variable_spec(inner_ty);
-                        let total_len = calculate_array_total_len(&ref_type.elem);
+                    arg_names.push(arg_name.clone());
 
-                        specs.push(quote! {
-                            IOVecSpec {
-                                len: #total_len,
-                                is_input: #is_input,
-                                is_output: #is_output,
-                            }
-                        });
+                    // 生成展开代码
+                    unflatten_code.push(generate_unflatten_code(
+                        current_index,
+                        &arg_name,
+                        &ref_type.elem
+                    ));
 
-                        let arg_name = quote! { #pat };
-                        arg_names.push(arg_name.clone());
+                    // 如果是输出变量，生成压平代码
+                    if is_output {
+                        flatten_code.push(generate_flatten_code(
+                            current_index,
+                            &arg_name,
+                            &ref_type.elem
+                        ));
+                    }
 
-                        let unflatten = generate_unflatten_code(
-                            &quote! { inputs[#current_index] },
-                            &ref_type.elem,
-                            &quote! { 0 },
-                        );
-                        unflatten_code.push(quote! {
-                            let #pat = #unflatten;
-                        });
+                    current_index += 1;
 
-                        if is_output {
-                            let flatten = generate_flatten_code(&arg_name, &ref_type.elem);
-                            flatten_code.push(quote! {
-                                inputs[#current_index] = #flatten;
-                            });
-                        }
-
-                        current_index += 1;
-
-                        let vec_type = replace_array_with_vec(&ref_type.elem);
-                        if ref_type.mutability.is_some() {
-                            quote! { #pat: &mut #vec_type }
-                        } else {
-                            quote! { #pat: &#vec_type }
-                        }
+                    arg_mutability.push(ref_type.mutability.is_some());
+                    if ref_type.mutability.is_some() {
+                        quote! { #pat: &mut #vec_type }
                     } else {
-                        panic!("Expected an array type for kernel parameters");
+                        quote! { #pat: &#vec_type }
                     }
                 } else {
-                    panic!("Expected a reference type for kernel parameters");
+                    panic!("Expected a reference to a multi-dimensional array for kernel parameters");
                 }
             } else {
-                panic!("Unsupported argument type for kernel function");
+                panic!("Expected a reference type for kernel parameters");
             }
-        })
-        .collect();
+        } else {
+            panic!("Unsupported argument type for kernel function");
+        }
+    }).collect();
+
+    // 生成函数参数引用
+    let fn_args = arg_names
+        .iter()
+        .zip(arg_mutability.iter())
+        .map(|(name, is_mut)| {
+            if *is_mut {
+                quote! { &mut #name }
+            } else {
+                quote! { &#name }
+            }
+        });
 
     let expanded = quote! {
         fn #user_fn_name<C: Config>(#api_arg, #(#user_fn_inputs),*) {
             #(#stmts)*
         }
 
-        fn #compile_fn_name<C: Config>() -> Kernel<C> {
+        fn #compile_fn_name<C: Config>() -> Result<Kernel<C>, Error> {
             compile_with_spec(
                 |api: &mut API<C>, inputs: &mut Vec<Vec<Variable>>| {
                     #(#unflatten_code)*
 
-                    #user_fn_name(api, #(&#arg_names),*);
+                    #user_fn_name(api, #(#fn_args),*);
 
                     #(#flatten_code)*
                 },
                 &[#(#specs),*]
             )
-            .unwrap()
         }
     };
 
+    // eprintln!("Expanded tokens: {:#?}", expanded);
+    // eprintln!("Expanded code: {}", expanded);
     TokenStream::from(expanded)
 }
--- expander_compiler/tests/zkcuda_1.rs
+++ expander_compiler/tests/zkcuda_1.rs
@@ -17,18 +17,6 @@ fn add_16<C: Config>(api: &mut API<C>, inputs: &mut Vec<Vec<Variable>>) {
     inputs[1][0] = sum;
 }
 
-type InputVariable = Variable;
-type OutputVariable = Variable;
-
-#[kernel]
-fn process_multidim<C: Config>(
-    api: &mut API<C>,
-    a: &[InputVariable; 2],
-    b: &mut [OutputVariable; 1],
-) {
-    b[0] = api.add(a[0], a[1]);
-}
-
 #[test]
 fn zkcuda_1() {
     let kernel_add_2: Kernel<M31Config> = compile_with_spec(
@@ -63,6 +51,56 @@ fn zkcuda_1() {
         ],
     )
     .unwrap();
+
+    let mut ctx: Context<M31Config, DummyProvingSystem<M31Config>> = Context::default();
+    let mut a = vec![];
+    for i in 0..32 {
+        a.push(M31::from(i + 1 as u32));
+    }
+    let a = ctx.copy_to_device(&a);
+    let mut io = vec![Some(a), None];
+    ctx.call_kernel(&kernel_add_2, &mut io, 16, &vec![false, false]);
+    let b = io[1].unwrap();
+    let mut io = vec![Some(b), None];
+    ctx.call_kernel(&kernel_add_16, &mut io, 1, &vec![false, false]);
+    let c = io[1].unwrap();
+    let result = ctx.copy_to_host(c);
+    assert_eq!(result, vec![M31::from(32 * 33 / 2)]);
+    let proof = ctx.to_proof();
+    assert!(proof.verify());
+}
+
+#[kernel]
+fn macro_kernel<C: Config>(
+    api: &mut API<C>,
+    a: &[[InputVariable; 4]; 2],
+    b: &mut [[OutputVariable; 1]; 4],
+    c: &mut [[[InputOutputVariable; 2]; 1]; 4],
+) {
+    for i in 0..4 {
+        b[i][0] = api.add(a[0][i], a[1][i]);
+        c[i][0][0] = api.add(c[i][0][0], c[i][0][1]);
+    }
+}
+
+#[kernel]
+fn add_2_macro<C: Config>(api: &mut API<C>, a: &[InputVariable; 2], b: &mut [OutputVariable; 1]) {
+    b[0] = api.add(a[0], a[1]);
+}
+
+#[kernel]
+fn add_16_macro<C: Config>(api: &mut API<C>, a: &[InputVariable; 16], b: &mut [OutputVariable; 1]) {
+    let mut sum = api.constant(0);
+    for i in 0..16 {
+        sum = api.add(sum, a[i]);
+    }
+    b[0] = sum;
+}
+
+#[test]
+fn zkcuda_2() {
+    let kernel_add_2: Kernel<M31Config> = compile_add_2_macro().unwrap();
+    let kernel_add_16: Kernel<M31Config> = compile_add_16_macro().unwrap();
 
     let mut ctx: Context<M31Config, DummyProvingSystem<M31Config>> = Context::default();
     let mut a = vec![];
