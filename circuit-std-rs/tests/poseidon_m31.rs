f896c8208a4f78ba13622fea3ce839cddb96d048
Hang Su <tonyfloater@gmail.com>
2025-01-16 19:46:15 -0500

    rename tests and add comments from review

--- circuit-std-rs/tests/poseidon_m31.rs
+++ circuit-std-rs/tests/poseidon_m31.rs
@@ -21,7 +21,8 @@ impl Define<M31Config> for PoseidonSpongeLen8Circuit<Variable> {
 }
 
 #[test]
-fn test_poseidon_m31x16_hash_to_state_len8() {
+// NOTE(HS) Poseidon Mersenne-31 Width-16 Sponge tested over input length 8
+fn test_poseidon_m31x16_hash_to_state_input_len8() {
     let compile_result = compile(&PoseidonSpongeLen8Circuit::default()).unwrap();
 
     let assignment = PoseidonSpongeLen8Circuit::<M31> {
@@ -73,7 +74,8 @@ impl Define<M31Config> for PoseidonSpongeLen16Circuit<Variable> {
 }
 
 #[test]
-fn test_poseidon_m31x16_hash_to_state_len16() {
+// NOTE(HS) Poseidon Mersenne-31 Width-16 Sponge tested over input length 16
+fn test_poseidon_m31x16_hash_to_state_input_len16() {
     let compile_result = compile(&PoseidonSpongeLen16Circuit::default()).unwrap();
 
     let assignment = PoseidonSpongeLen16Circuit::<M31> {
