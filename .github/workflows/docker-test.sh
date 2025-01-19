301604154c4d32d3fd34c62752139237423a792f
siq1 <siqi@polyhedra.network>
2025-01-19 14:28:15 +0000

    use docker for 7950x ci

---
+++ .github/workflows/docker-test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cp /tmp/ecc2/ecc2 /tmp/ecc-test -r
+cd /tmp/ecc-test
+apt-get update
+apt-get install -y libopenmpi-dev clang
+
+case $1 in
+    "test-rust-avx512")
+        RUSTFLAGS="-C target-cpu=native -C target-feature=+avx512f" cargo test
+        ;;
+esac
