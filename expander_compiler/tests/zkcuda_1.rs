44b71cfa9411a7ce6b720b6ff76f10c4ebc3ae40
siq1 <siqi@polyhedra.network>
2025-01-24 02:20:45 +0000

    add tests

--- expander_compiler/tests/zkcuda_1.rs
+++ expander_compiler/tests/zkcuda_1.rs
@@ -111,3 +111,37 @@ fn zkcuda_2() {
     let proof = ctx.to_proof();
     assert!(proof.verify());
 }
+
+#[test]
+fn zkcuda_2_simd() {
+    use arith::SimdField;
+    let kernel_add_2: Kernel<M31Config> = compile_add_2_macro().unwrap();
+    let kernel_add_16: Kernel<M31Config> = compile_add_16_macro().unwrap();
+
+    let mut ctx: Context<M31Config, DummyProvingSystem<M31Config>> = Context::default();
+    let mut a: Vec<Vec<mersenne31::M31x16>> = vec![];
+    for i in 0..16 {
+        a.push(vec![]);
+        for j in 0..2 {
+            let mut tmp = Vec::new();
+            for k in 0..16 {
+                tmp.push(M31::from((i * 2 + j + 1 + k) as u32));
+            }
+            a[i].push(mersenne31::M31x16::pack(&tmp));
+        }
+    }
+    let a = ctx.copy_simd_to_device(&a, false);
+    let mut b = None;
+    call_kernel!(ctx, kernel_add_2, a, mut b);
+    let b = b.reshape(&[1, 16]);
+    let mut c = None;
+    call_kernel!(ctx, kernel_add_16, b, mut c);
+    let c = c.reshape(&[]);
+    let result: mersenne31::M31x16 = ctx.copy_simd_to_host(c);
+    let result = result.unpack();
+    for k in 0..16 {
+        assert_eq!(result[k], M31::from((32 * 33 / 2 + 32 * k) as u32));
+    }
+    let proof = ctx.to_proof();
+    assert!(proof.verify());
+}
