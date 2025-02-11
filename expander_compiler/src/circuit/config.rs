7a0381bb463470fe49e0f45139cc1a99b5e3924d
zhenfei <zhenfei.zhang@hotmail.com>
2025-02-11 18:36:34 -0500

    fix gkr config

--- expander_compiler/src/circuit/config.rs
+++ expander_compiler/src/circuit/config.rs
@@ -31,7 +31,7 @@ impl Config for M31Config {
 
     type DefaultSimdField = mersenne31::M31x16;
     type DefaultGKRFieldConfig = gkr_field_config::M31ExtConfig;
-    type DefaultGKRConfig = gkr::executor::M31ExtConfigSha2;
+    type DefaultGKRConfig = gkr::gkr_configs::M31ExtConfigKeccakRaw;
 
     const CONFIG_ID: usize = 1;
 }
@@ -44,7 +44,7 @@ impl Config for BN254Config {
 
     type DefaultSimdField = crate::field::BN254;
     type DefaultGKRFieldConfig = gkr_field_config::BN254Config;
-    type DefaultGKRConfig = gkr::executor::BN254ConfigMIMC5;
+    type DefaultGKRConfig = gkr::gkr_configs::BN254ConfigMIMC5Raw;
 
     const CONFIG_ID: usize = 2;
 }
@@ -57,7 +57,7 @@ impl Config for GF2Config {
 
     type DefaultSimdField = gf2::GF2x8;
     type DefaultGKRFieldConfig = gkr_field_config::GF2ExtConfig;
-    type DefaultGKRConfig = gkr::executor::GF2ExtConfigSha2;
+    type DefaultGKRConfig = gkr::gkr_configs::GF2ExtConfigSha2Orion;
 
     const CONFIG_ID: usize = 3;
 
