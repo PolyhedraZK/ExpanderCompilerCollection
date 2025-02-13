145f45403a7a63f9ce90cae5e5f83b2e32cd4966
Zhiyong Fang <zhiyong.fang.1997@gmail.com>
2025-02-12 21:26:24 -0600

    add some comments to zkcuda interface

--- expander_compiler/src/zkcuda/proving_system/traits.rs
+++ expander_compiler/src/zkcuda/proving_system/traits.rs
@@ -15,6 +15,16 @@ pub trait ProvingSystem<C: Config> {
     type Proof: Proof;
     type Commitment: Commitment<C>;
     fn commit(vals: &[C::DefaultSimdField]) -> Self::Commitment;
+
+    /// `kernel`: contains some basic information such as circuit structure.
+    /// `commitments`: the components/values that constitute the input to the circuit, 
+    ///     In case of parallel_count = 1: the sum of the lengths of the commitments must be equal to 
+    ///         the total number of inputs to the circuit
+    ///     In case of parallel_count > 1: the length of each component is multiplied by parallel_count
+    ///         if the corresponding is_broadcast is false
+    /// `parallel_count`: the number of instances of the circuit that are being run in parallel
+    /// `is_broadcast`: must have the same length as `commitments`, indicating whether the corresponding 
+    ///     component is the same across all the `parallel_count` instances of the circuit
     fn prove(
         kernel: &Kernel<C>,
         commitments: &[&Self::Commitment],
