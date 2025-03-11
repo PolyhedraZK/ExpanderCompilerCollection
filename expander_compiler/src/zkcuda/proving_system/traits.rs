8379e4eaedc882fc4410718fd4897cfb03840da5
Zhiyong Fang <zhiyong.fang.1997@gmail.com>
2025-03-10 21:56:08 -0500

    api change

--- expander_compiler/src/zkcuda/proving_system/traits.rs
+++ expander_compiler/src/zkcuda/proving_system/traits.rs
@@ -1,26 +1,28 @@
 use super::super::kernel::Kernel;
 use crate::circuit::config::Config;
 
-pub trait Commitment<C: Config>: Clone {
-    fn vals_ref(&self) -> &[C::DefaultSimdField];
-
-    fn vals_len(&self) -> usize {
-        self.vals_ref().len()
-    }
-}
-
-pub trait Proof: Clone {}
-
 pub trait ProvingSystem<C: Config> {
-    type Proof: Proof;
-    type Commitment: Commitment<C>;
-    fn commit(vals: &[C::DefaultSimdField]) -> Self::Commitment;
+    type ProverSetup: Clone;
+    type VerifierSetup: Clone;
+    type Proof: Clone;
+    type Commitment: Clone;
+    type CommitmentExtraInfo: Clone;
+
+    // TODO: the setup function should take the problem description as an input, 
+    // probably the context or all the kernels?
+    fn setup() -> (Self::ProverSetup, Self::VerifierSetup); 
+    
+    fn commit(vals: &[C::DefaultSimdField], prover_setup: &Self::ProverSetup) -> (Self::Commitment, Self::CommitmentExtraInfo);
     fn prove(
+        prover_setup: &Self::ProverSetup,
         kernel: &Kernel<C>,
+        vals: &[C::DefaultSimdField],
         commitments: &[&Self::Commitment],
+        commitments_extra_info: &[Self::CommitmentExtraInfo],
         parallel_count: usize,
         is_broadcast: &[bool],
     ) -> Self::Proof;
+
     fn verify(
         kernel: &Kernel<C>,
         proof: &Self::Proof,
