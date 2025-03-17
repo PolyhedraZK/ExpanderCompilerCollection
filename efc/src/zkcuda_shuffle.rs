f45eb0d96035ba751f5a97ced1c233dd4f9713d7
strilingx001 <yue@polyhedra.network>
2025-03-17 17:23:25 +0800

    implement zkcuda_shuffle

--- efc/src/zkcuda_shuffle.rs
+++ efc/src/zkcuda_shuffle.rs
@@ -5,20 +5,12 @@ use crate::shuffle::{
     MAX_VALIDATOR_EXP, POSEIDON_HASH_LENGTH, SHUFFLE_ROUND, VALIDATOR_CHUNK_SIZE,
 };
 use crate::validator::ValidatorSSZ;
-use crate::zkcuda_hashtable::{HASHTABLESIZE, SHA256LEN};
 use ark_std::iterable::Iterable;
 use circuit_std_rs::gnark::emulated::sw_bls12381::g1::*;
 use circuit_std_rs::gnark::emulated::sw_bls12381::g2::*;
-use circuit_std_rs::gnark::emulated::sw_bls12381::pairing::*;
-use circuit_std_rs::sha256::m31::sha256_37bytes;
-use expander_compiler::frontend::M31Config;
-use expander_compiler::frontend::*;
 use expander_compiler::frontend::*;
 use expander_compiler::zkcuda::context::{call_kernel, Context, Reshape};
-use expander_compiler::zkcuda::kernel::Kernel;
 use expander_compiler::zkcuda::kernel::*;
-use expander_compiler::zkcuda::proving_system::ExpanderGKRProvingSystem;
-use mersenne31::M31;
 use circuit_std_rs::utils::simple_select;
 
 fn sub_vector<T>(vec: &Vec<T>, start: usize, length: usize) -> (Vec<T>, usize)
@@ -40,7 +32,7 @@ fn shuffle_inner<C: Config>(api: &mut API<C>, p: &Vec<Variable>) -> Vec<Variable
     let (start_index, pos) = sub_vector(p, 0, 1);
     let (chunk_length, pos) = sub_vector(p, pos, 1);
     let (shuffle_indices, pos) = sub_vector(p, pos, VALIDATOR_CHUNK_SIZE);
-    let (committee_indices, pos) = sub_vector(p, pos, VALIDATOR_CHUNK_SIZE);
+    let (_, pos) = sub_vector(p, pos, VALIDATOR_CHUNK_SIZE);
     let (pivots, pos) = sub_vector(p, pos, SHUFFLE_ROUND);
     let (index_count, pos) = sub_vector(p, pos, 1);
     let (position_results, pos) = sub_vector(p, pos, SHUFFLE_ROUND * VALIDATOR_CHUNK_SIZE);
@@ -71,7 +63,7 @@ fn shuffle_inner<C: Config>(api: &mut API<C>, p: &Vec<Variable>) -> Vec<Variable
     let (activation_eligibility_epoch, pos) = sub_vector(p, pos, 8 * VALIDATOR_CHUNK_SIZE);
     let (activation_epoch, pos) = sub_vector(p, pos, 8 * VALIDATOR_CHUNK_SIZE);
     let (exit_epoch, pos) = sub_vector(p, pos, 8 * VALIDATOR_CHUNK_SIZE);
-    let (withdrawable_epoch, _pos) = sub_vector(p, pos, 8 * VALIDATOR_CHUNK_SIZE);
+    let (withdrawable_epoch, _) = sub_vector(p, pos, 8 * VALIDATOR_CHUNK_SIZE);
 
     //println!("pos: {}", pos);
     //api.assert_is_equal(p[0], 1);
@@ -259,6 +251,6 @@ fn compute_shuffle<C: Config>(
 }
 #[test]
 fn test_zkcuda_shuffle() {
-    let kernel: Kernel<M31Config> = compile_compute_shuffle().unwrap();
+    let _: Kernel<M31Config> = compile_compute_shuffle().unwrap();
     println!("compile ok");
 }
