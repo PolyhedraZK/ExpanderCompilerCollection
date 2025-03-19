dfd7d3adef9adf967cd782106c68d61596a31bf1
hczphn <huancheng@polyhedra.network>
2025-03-19 02:50:32 +0000

    add generate hashtable api, locally calculate hashtable asisgnment

--- efc/src/beacon.rs
+++ efc/src/beacon.rs
@@ -1,8 +1,8 @@
 //read beacon info from files
-use std::{fs::{self, File}, io::BufReader, error::Error};
+use std::{error::Error, fs};
 use std::path::Path;
+use byteorder::{ByteOrder, LittleEndian};
 
-use serde::Deserialize;
 use sha2::{Digest, Sha256};
 
 const SUBCIRCUIT_TREE_CACHE_DIR: &str = "./data/subcircuitTreeCache/";
@@ -47,13 +47,34 @@ pub fn get_beacon_seed(epoch: u64) -> Option<Vec<u8>> {
 
 pub fn get_randao_from_slot(slot: u64) -> Result<Vec<u8>, Box<dyn Error>> {
     let path = format!("{}{}.json", RANDAO_DIR, slot);
-    println!("path: {}", path);
     let json_content = fs::read_to_string(path)?;
     let randao_hex: String = serde_json::from_str(&json_content)?;
     let randao_bytes = hex::decode(randao_hex)?;
     Ok(randao_bytes)
 }
 
+pub fn generate_hash_table(seed: &[u8], count: usize, shuffle_round: usize) -> Vec<[u8; 32]> {
+    let count_exp = (count as f64).log2().ceil() as usize;
+    let adjusted_count = 1 << count_exp;
+    let size_per_round = (adjusted_count + 255) / 256;
+    let table_size = shuffle_round * size_per_round;
+
+    let mut table_inputs = vec![vec![0u8; 37]; table_size]; // MaxInputLength assumed 64
+
+    for i in 0..table_size {
+        table_inputs[i][..32].copy_from_slice(&seed[..32]);
+        table_inputs[i][32] = (i / size_per_round) as u8;
+        LittleEndian::write_u32(&mut table_inputs[i][33..37], (i % size_per_round) as u32);
+    }
+    table_inputs.iter()
+        .map(|input| {
+            let hash = Sha256::digest(input);
+            let mut result = [0u8; 32];
+            result.copy_from_slice(&hash);
+            result
+        })
+        .collect()
+}
 #[test]
 fn test_get_beacon_seed() {
     init_directories().unwrap();
