c6fbcdafbc694af1af070576aad3177a98d76f86
hczphn <huancheng@polyhedra.network>
2025-03-19 04:01:06 +0000

    add shuffle indices api

--- efc/src/beacon.rs
+++ efc/src/beacon.rs
@@ -85,6 +85,70 @@ pub fn get_activated_validator_indices(slot: u64) -> Result<Vec<u64>, Box<dyn Er
     let activated_validator_indices: Vec<u64> = serde_json::from_str(&json_content)?;
     Ok(activated_validator_indices)
 }
+
+fn shuffle_index(index: u64, index_count: u64, seed: &[u8], round: usize, bits: &[u8]) -> (u64, Vec<u64>, Vec<u64>, Vec<u64>, Vec<u64>, Vec<u8>) {
+    let mut flips = vec![0u64; round];
+    let mut positions = vec![0u64; round];
+    let mut flip_bits = vec![0u8; round];
+    let mut pivots = vec![0u64; round];
+    let mut round_index = vec![0u64; round];
+    let mut current_index = index;
+
+    for current_round in 0..round {
+        round_index[current_round] = current_index;
+        let round_byte = current_round as u8;
+        let mut to_hash = [seed, &[round_byte]].concat();
+        let hash_res = Sha256::digest(&to_hash);
+
+        let pivot = LittleEndian::read_u64(&hash_res[0..8]) % index_count;
+        pivots[current_round] = pivot;
+
+        let flip = (pivot + index_count - current_index) % index_count;
+        let position = current_index.max(flip);
+        let position_div = (position / 256) as u32;
+        let mut position_bytes = [0u8; 4];
+        LittleEndian::write_u32(&mut position_bytes, position_div);
+
+        to_hash = [seed, &[round_byte], &position_bytes].concat();
+        let source = Sha256::digest(&to_hash);
+        let source_byte = source[((position % 256) / 8) as usize];
+        let bit = (source_byte >> (position % 8)) % 2;
+
+        if bit != bits[position as usize + current_round * bits.len() / round] {
+            panic!("bit not equal");
+        }
+        if bit == 1 {
+            current_index = flip;
+        }
+        flips[current_round] = flip;
+        positions[current_round] = position;
+        flip_bits[current_round] = bit;
+    }
+
+    (current_index, flips, positions, pivots, round_index, flip_bits)
+}
+
+pub fn shuffle_indices(indices: &[u64], seed: &[u8], bits: &[u8], shuffle_round: usize) -> (Vec<u64>, Vec<Vec<u64>>, Vec<Vec<u64>>, Vec<Vec<u64>>, Vec<u64>, Vec<Vec<u8>>) {
+    let mut shuffle_indices = vec![0u64; indices.len()];
+    let mut flips = vec![vec![]; indices.len()];
+    let mut positions = vec![vec![]; indices.len()];
+    let mut flip_bits = vec![vec![]; indices.len()];
+    let mut pivots = vec![0u64; indices.len()];
+    let mut shuffle_round_indices = vec![vec![]; indices.len()];
+
+    for i in 0..indices.len() {
+        let (shuffled_index, f, pos, piv, round_idx, flip_b) = shuffle_index(i as u64, indices.len() as u64, seed, shuffle_round, bits);
+        shuffle_indices[i] = shuffled_index;
+        flips[i] = f;
+        positions[i] = pos;
+        pivots = piv;
+        shuffle_round_indices[i] = round_idx;
+        flip_bits[i] = flip_b;
+    }
+
+    (shuffle_indices, flips, positions, shuffle_round_indices, pivots, flip_bits)
+}
+
 #[test]
 fn test_get_beacon_seed() {
     init_directories().unwrap();
@@ -97,4 +161,27 @@ fn test_get_beacon_seed() {
 fn test_get_activated_validator_indices() {
     let indices = get_activated_validator_indices(3988672).unwrap();
     println!("{:?}", indices.len());
+}
+
+#[test]
+fn test_shuffle_indices(){
+    let indices = get_activated_validator_indices(3988672).unwrap();
+    let seed = get_beacon_seed(124646).unwrap();
+    let hash_bytes = generate_hash_table(&seed, indices.len(), 90);
+    let mut hash_bits = vec![0u8; hash_bytes.len() * 256];
+
+    for (i, hash_byte) in hash_bytes.iter().enumerate() {
+        for j in 0..32 {
+            for k in 0..8 {
+                hash_bits[i * 256 + j * 8 + k] = (hash_byte[j] >> k) & 1;
+            }
+        }
+    }
+    let (shuffle_indices, flips, positions, shuffle_round_indices, pivots, flip_bits) = shuffle_indices(&indices, &seed, &hash_bits, 90);
+    // println!("{:?}", shuffle_indices);
+    // println!("{:?}", flips);
+    // println!("{:?}", positions);
+    // println!("{:?}", shuffle_round_indices);
+    // println!("{:?}", pivots);
+    // println!("{:?}", flip_bits);
 }
