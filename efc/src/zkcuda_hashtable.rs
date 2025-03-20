bf4576092ef334345b809df01eed23e2acccd457
stirlingx001 <yue@polyhedra.network>
2025-03-20 11:35:37 +0800

    test

--- efc/src/zkcuda_hashtable.rs
+++ efc/src/zkcuda_hashtable.rs
@@ -1,3 +1,4 @@
+use crate::hashtable::{HASHTABLESIZE, SHA256LEN};
 use circuit_std_rs::sha256::m31::sha256_37bytes;
 use circuit_std_rs::sha256::m31_utils::{big_array_add, to_binary_hint};
 use expander_compiler::frontend::*;
@@ -5,8 +6,6 @@ use expander_compiler::zkcuda::context::{call_kernel, Context};
 use expander_compiler::zkcuda::kernel::*;
 use expander_compiler::zkcuda::proving_system::ExpanderGKRProvingSystem;
 
-pub const SHA256LEN: usize = 32;
-pub const HASHTABLESIZE: usize = 2;
 #[allow(dead_code)]
 fn compute_hashtable_inner<C: Config>(api: &mut API<C>, p: &Vec<Variable>) -> Vec<Variable> {
     let shuffle_round = p[0];
@@ -14,16 +13,12 @@ fn compute_hashtable_inner<C: Config>(api: &mut API<C>, p: &Vec<Variable>) -> Ve
     let seed = &p[5..];
     let mut output = vec![];
 
-    let mut indices = vec![Vec::<Variable>::new(); crate::hashtable::HASHTABLESIZE];
-    if crate::hashtable::HASHTABLESIZE > 256 {
+    let mut indices = vec![Vec::<Variable>::new(); HASHTABLESIZE];
+    if HASHTABLESIZE > 256 {
         panic!("HASHTABLESIZE > 256")
     }
     let var0 = api.constant(0);
-    for (i, cur_index) in indices
-        .iter_mut()
-        .enumerate()
-        .take(crate::hashtable::HASHTABLESIZE)
-    {
+    for (i, cur_index) in indices.iter_mut().enumerate().take(HASHTABLESIZE) {
         //assume HASHTABLESIZE is less than 2^8
         let var_i = api.constant(i as u32);
         let index = big_array_add(api, &start_index, &[var_i, var0, var0, var0], 8);
@@ -53,14 +48,13 @@ fn compute_hashtable<C: Config>(
     let outc = api.memorized_simple_call(compute_hashtable_inner, input);
     for i in 0..HASHTABLESIZE {
         for j in 0..SHA256LEN {
-            output[i*SHA256LEN + j] = outc[i * SHA256LEN + j]
+            output[i * SHA256LEN + j] = outc[i * SHA256LEN + j]
         }
     }
 }
 
 //#[test]
 pub fn test_zkcuda_hashtable() {
-
     let mut hint_registry = HintRegistry::<M31>::new();
     hint_registry.register("myhint.tobinary", to_binary_hint);
 
@@ -76,7 +70,7 @@ pub fn test_zkcuda_hashtable() {
 
     let mut p: Vec<M31> = vec![];
     p.push(M31::from(shuffle_round));
-    for i in 0.. 4 {
+    for i in 0..4 {
         p.push(M31::from(start_index[i]))
     }
     for i in 0..SHA256LEN {
@@ -87,7 +81,7 @@ pub fn test_zkcuda_hashtable() {
     let p = ctx.copy_to_device(&vec![p], false);
 
     println!("copy to device ok");
-   // println!("p: {:?}", p.clone().unwrap().shape.unwrap());
+    // println!("p: {:?}", p.clone().unwrap().shape.unwrap());
 
     let start_time = std::time::Instant::now();
     let kernel: Kernel<M31Config> = compile_compute_hashtable().unwrap();
@@ -100,9 +94,9 @@ pub fn test_zkcuda_hashtable() {
     let t3 = std::time::Instant::now();
     println!("call kernel ok, time {:?}", t3.duration_since(t2));
 
-   // println!("out shape: {:?}", out.clone().unwrap().shape.unwrap());
-   // let out = out.reshape(&[SHA256LEN*HASHTABLESIZE]);
-   // println!("out shape: {:?}", out.clone().unwrap().shape.unwrap());
+    // println!("out shape: {:?}", out.clone().unwrap().shape.unwrap());
+    // let out = out.reshape(&[SHA256LEN*HASHTABLESIZE]);
+    // println!("out shape: {:?}", out.clone().unwrap().shape.unwrap());
 
     //let out: Vec<M31> = ctx.copy_to_host(out);
     //println!("copy to host ok");
