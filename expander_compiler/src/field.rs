3ad93a6fe1db1ecf5f3013204bdc6913a9290d6b
siq1 <siqi@polyhedra.network>
2025-03-21 00:16:14 +0000

    rename fr to bn254

--- expander_compiler/src/field.rs
+++ expander_compiler/src/field.rs
@@ -1,10 +1,10 @@
-pub use arith::{Field as FieldArith, Fr};
+pub use arith::{Field as FieldArith, Fr as BN254};
 pub use gf2::GF2;
 pub use mersenne31::M31;
 use serdes::ExpSerde;
 
 pub trait Field: FieldArith + ExpSerde {}
 
-impl Field for Fr {}
+impl Field for BN254 {}
 impl Field for GF2 {}
 impl Field for M31 {}
