0fae7e17b6916f4c706e6034bb80674f9a6a11f6
hczphn <huancheng@polyhedra.network>
2025-03-26 04:59:13 +0000

    update end2end api

--- efc/src/main.rs
+++ efc/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use efc::end2end::end2end_witness_streamline;
+use efc::end2end::end2end_witness_streamline_from_beacon_data;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,7 +9,7 @@ fn main() {
         if let Some(stage) = args.get(f_index + 1) {
             println!("The stage of -s is: {}", stage);
             stacker::grow(32 * 1024 * 1024 * 1024, || {
-                end2end_witness_streamline(stage);
+                end2end_witness_streamline_from_beacon_data(290000, stage);
             });
         } else {
             println!("stage must be specified");
