a38d84c23150f03080e85eb6d4c96ed7f66a5fbd
hczphn <huancheng@polyhedra.network>
2025-04-02 03:30:43 +0000

    add debug when preparing

--- efc/prepare_solver.sh
+++ efc/prepare_solver.sh
@@ -16,25 +16,28 @@ for circuit in shuffle permutationhash permutationquery hashtable blsverifier va
     sleep 1
 done
 
-# 4. Clone the repo
+# 4. Debug all circuits on a default epoch
+./efc -d 290001 &
+
+# 5. Clone the repo
 cd ../../ || { echo "Failed to enter parent parent directory"; exit 1; }
 rm -rf EthFullConsensus
 git clone https://github.com/PolyhedraZK/EthFullConsensus.git
 sleep 5
-# 5. Enter the repo and checkout the branch
+# 6. Enter the repo and checkout the branch
 cd EthFullConsensus || { echo "Failed to enter EthFullConsensus"; exit 1; }
 sleep 1
 git checkout dev_pcs
 
-# 6. Build the Go program
+# 7. Build the Go program
 cd end2end/cmd || { echo "Go cmd dir not found"; exit 1; }
 go build -o cmd || { echo "Go build failed"; exit 1; }
 sleep 10
-# 7. Move the compiled Go program to the original directory
+# 8. Move the compiled Go program to the original directory
 mv cmd "$ROOT_DIR/"
 
-# 8. Go back to efc root
+# 9. Go back to efc root
 cd "$ROOT_DIR" || exit 1
-# 9. Optional: Wait for all background processes to complete
+# 10. Optional: Wait for all background processes to complete
 wait
 echo "All background processes finished."
