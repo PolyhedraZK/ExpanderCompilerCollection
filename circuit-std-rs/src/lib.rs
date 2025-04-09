0796ef73d1081a860ea4490afb65743b4bd05771
siq1 <siqi@polyhedra.network>
2025-04-09 16:18:02 +0000

    remove sha2 since we already have other impls

--- circuit-std-rs/src/lib.rs
+++ circuit-std-rs/src/lib.rs
@@ -10,9 +10,6 @@ pub use logup::{LogUpCircuit, LogUpParams};
 pub mod non_native;
 pub use non_native::*;
 
-pub mod sha2;
-pub use sha2::*;
-
 pub mod big_int;
 pub mod sha2_m31;
 #[cfg(test)]
--- circuit-std-rs/src/tests.rs
+++ circuit-std-rs/src/tests.rs
@@ -1,6 +1,5 @@
 mod common;
 mod logup;
-mod sha2;
 mod u120;
 mod u2048;
 mod util;
