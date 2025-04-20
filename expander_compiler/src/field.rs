1fdbc8a58f6038392d45745bb9ee9f526e812dc5
zhenfei <zhenfei.zhang@hotmail.com>
2025-04-20 10:17:19 -0700

    impl babybear (#138)
    * wip
    
    * wip
    
    * fix
    
    * builds
    
    * everything works except for goldilocks
    
    * goldilocks also works now
    
    * wip
    
    * finished
    
    * update cargo lock
    
    * update cargo lock
    
    * Update Cargo.lock
    
    * fix clippy

--- expander_compiler/src/field.rs
+++ expander_compiler/src/field.rs
@@ -1,4 +1,5 @@
 pub use arith::{Field as FieldArith, Fr as BN254Fr};
+use babybear::BabyBear;
 pub use gf2::GF2;
 pub use goldilocks::Goldilocks;
 pub use mersenne31::M31;
@@ -10,3 +11,4 @@ impl Field for BN254Fr {}
 impl Field for GF2 {}
 impl Field for M31 {}
 impl Field for Goldilocks {}
+impl Field for BabyBear {}
