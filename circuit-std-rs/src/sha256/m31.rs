5cf0ca61b3c6f424006126ca74e7c131a55220b3
hczphn <huancheng@polyhedra.network>
2025-05-01 02:23:36 +0000

    make my digest public

--- circuit-std-rs/src/sha256/m31.rs
+++ circuit-std-rs/src/sha256/m31.rs
@@ -41,7 +41,7 @@ const _K: [u32; 64] = [
     0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
 ];
-struct MyDigest {
+pub struct MyDigest {
     h: [[Variable; 2]; 8],
     nx: usize,
     len: u64,
