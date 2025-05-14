ad4b6ed7c2af51c06dcccb1bd5d4ec099229388c
zhiyong <zhiyong.fang.1997@gmail.com>
2025-05-14 00:48:16 +0000

    move temporary files to /tmp

--- expander_compiler/src/zkcuda/proving_system/callee_utils.rs
+++ expander_compiler/src/zkcuda/proving_system/callee_utils.rs
@@ -27,14 +27,14 @@ pub fn read_object_from_shared_memory_name_string<T: ExpSerde>(
 #[allow(clippy::type_complexity)]
 pub fn read_selected_pkey_from_shared_memory<F: FieldEngine, PCS: ExpanderPCS<F>>(
 ) -> (usize, <PCS::SRS as StructuredReferenceString>::PKey) {
-    read_object_from_shared_memory_name_string("pcs_setup", 0)
+    read_object_from_shared_memory_name_string("/tmp/pcs_setup", 0)
 }
 
 pub fn read_local_vals_to_commit_from_shared_memory<F: FieldEngine>(
     world_rank: usize,
     world_size: usize,
 ) -> Vec<F::SimdCircuitField> {
-    let shmem = ShmemConf::new().flink("input_vals").open().unwrap();
+    let shmem = ShmemConf::new().flink("/tmp/input_vals").open().unwrap();
     let ptr = shmem.as_ptr();
     let total_len: usize =
         usize::deserialize_from(unsafe { std::slice::from_raw_parts(ptr, size_of::<usize>()) })
@@ -75,36 +75,36 @@ pub fn write_object_to_shared_memory_name_string<T: ExpSerde>(object: &T, shared
 pub fn write_commitment_to_shared_memory<F: FieldEngine, PCS: ExpanderPCS<F>>(
     commitment: &ExpanderGKRCommitment<F, PCS>,
 ) {
-    write_object_to_shared_memory_name_string(commitment, "commitment");
+    write_object_to_shared_memory_name_string(commitment, "/tmp/commitment");
 }
 
 pub fn write_commitment_extra_info_to_shared_memory<F: FieldEngine, PCS: ExpanderPCS<F>>(
     extra_info: &ExpanderGKRCommitmentExtraInfo<F, PCS>,
 ) {
-    write_object_to_shared_memory_name_string(extra_info, "extra_info");
+    write_object_to_shared_memory_name_string(extra_info, "/tmp/extra_info");
 }
 
 pub fn read_pcs_setup_from_shared_memory<F: FieldEngine, PCS: ExpanderPCS<F>>(
 ) -> ExpanderGKRProverSetup<F, PCS> {
-    read_object_from_shared_memory_name_string("pcs_setup", 0)
+    read_object_from_shared_memory_name_string("/tmp/pcs_setup", 0)
 }
 
 pub fn read_ecc_circuit_from_shared_memory<C: Config>() -> Circuit<C, NormalInputType> {
-    read_object_from_shared_memory_name_string("circuit", 0)
+    read_object_from_shared_memory_name_string("/tmp/circuit", 0)
 }
 
 pub fn read_partition_info_from_shared_memory() -> Vec<LayeredCircuitInputVec> {
-    read_object_from_shared_memory_name_string("input_partition", 0)
+    read_object_from_shared_memory_name_string("/tmp/input_partition", 0)
 }
 
 pub fn read_commitment_from_shared_memory<F: FieldEngine, PCS: ExpanderPCS<F>>(
 ) -> Vec<ExpanderGKRCommitment<F, PCS>> {
-    read_object_from_shared_memory_name_string("commitment", 0)
+    read_object_from_shared_memory_name_string("/tmp/commitment", 0)
 }
 
 pub fn read_commitment_extra_info_from_shared_memory<F: FieldEngine, PCS: ExpanderPCS<F>>(
 ) -> Vec<ExpanderGKRCommitmentExtraInfo<F, PCS>> {
-    read_object_from_shared_memory_name_string("extra_info", 0)
+    read_object_from_shared_memory_name_string("/tmp/extra_info", 0)
 }
 
 pub fn read_commitment_values_from_shared_memory<F: FieldEngine>(
@@ -112,7 +112,7 @@ pub fn read_commitment_values_from_shared_memory<F: FieldEngine>(
     world_rank: usize,
     world_size: usize,
 ) -> Vec<Vec<F::SimdCircuitField>> {
-    let shmem = ShmemConf::new().flink("input_vals").open().unwrap();
+    let shmem = ShmemConf::new().flink("/tmp/input_vals").open().unwrap();
     let mut ptr = shmem.as_ptr();
     let n_components: usize =
         usize::deserialize_from(unsafe { std::slice::from_raw_parts(ptr, size_of::<usize>()) })
@@ -163,9 +163,9 @@ pub fn read_commitment_values_from_shared_memory<F: FieldEngine>(
 }
 
 pub fn read_broadcast_info_from_shared_memory() -> Vec<bool> {
-    read_object_from_shared_memory_name_string("broadcast_info", 0)
+    read_object_from_shared_memory_name_string("/tmp/broadcast_info", 0)
 }
 
 pub fn write_proof_to_shared_memory(proof: &ExpanderGKRProof) {
-    write_object_to_shared_memory_name_string(proof, "proof");
+    write_object_to_shared_memory_name_string(proof, "/tmp/proof");
 }
