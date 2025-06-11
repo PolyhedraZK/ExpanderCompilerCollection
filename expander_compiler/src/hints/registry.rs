7220e6d60a483d1c44cd33e5ffd22c5312dd6b1a
siq1 <siqi@polyhedra.network>
2025-06-11 23:16:46 +0000

    change hints to fn

--- expander_compiler/src/hints/registry.rs
+++ expander_compiler/src/hints/registry.rs
@@ -6,11 +6,11 @@ use crate::{field::Field, utils::error::Error};
 
 use super::{stub_impl, BuiltinHintIds};
 
-pub type HintFn<F> = dyn FnMut(&[F], &mut [F]) -> Result<(), Error>;
+type HintFn<F> = fn(&[F], &mut [F]) -> Result<(), Error>;
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 pub struct HintRegistry<F: Field> {
-    hints: HashMap<usize, Box<HintFn<F>>>,
+    hints: HashMap<usize, HintFn<F>>,
 }
 
 pub fn hint_key_to_id(key: &str) -> usize {
@@ -30,19 +30,15 @@ impl<F: Field> HintRegistry<F> {
     pub fn new() -> Self {
         Self::default()
     }
-    pub fn register<Hint: Fn(&[F], &mut [F]) -> Result<(), Error> + 'static>(
-        &mut self,
-        key: &str,
-        hint: Hint,
-    ) {
+    pub fn register(&mut self, key: &str, hint: fn(&[F], &mut [F]) -> Result<(), Error>) {
         let id = hint_key_to_id(key);
         if self.hints.contains_key(&id) {
             panic!("Hint with id {id} already exists");
         }
-        self.hints.insert(id, Box::new(hint));
+        self.hints.insert(id, hint);
     }
-    pub fn call(&mut self, id: usize, args: &[F], num_outputs: usize) -> Result<Vec<F>, Error> {
-        if let Some(hint) = self.hints.get_mut(&id) {
+    pub fn call(&self, id: usize, args: &[F], num_outputs: usize) -> Result<Vec<F>, Error> {
+        if let Some(hint) = self.hints.get(&id) {
             let mut outputs = vec![F::zero(); num_outputs];
             hint(args, &mut outputs).map(|_| outputs)
         } else {
@@ -62,23 +58,23 @@ impl EmptyHintCaller {
 pub struct StubHintCaller;
 
 pub trait HintCaller<F: Field>: 'static {
-    fn call(&mut self, id: usize, args: &[F], num_outputs: usize) -> Result<Vec<F>, Error>;
+    fn call(&self, id: usize, args: &[F], num_outputs: usize) -> Result<Vec<F>, Error>;
 }
 
 impl<F: Field + 'static> HintCaller<F> for HintRegistry<F> {
-    fn call(&mut self, id: usize, args: &[F], num_outputs: usize) -> Result<Vec<F>, Error> {
+    fn call(&self, id: usize, args: &[F], num_outputs: usize) -> Result<Vec<F>, Error> {
         self.call(id, args, num_outputs)
     }
 }
 
 impl<F: Field> HintCaller<F> for EmptyHintCaller {
-    fn call(&mut self, id: usize, _: &[F], _: usize) -> Result<Vec<F>, Error> {
+    fn call(&self, id: usize, _: &[F], _: usize) -> Result<Vec<F>, Error> {
         Err(Error::UserError(format!("hint with id {id} not found")))
     }
 }
 
 impl<F: Field> HintCaller<F> for StubHintCaller {
-    fn call(&mut self, id: usize, args: &[F], num_outputs: usize) -> Result<Vec<F>, Error> {
+    fn call(&self, id: usize, args: &[F], num_outputs: usize) -> Result<Vec<F>, Error> {
         Ok(stub_impl(id, &args.to_vec(), num_outputs))
     }
 }
--- expander_compiler/tests/to_binary_hint.rs
+++ expander_compiler/tests/to_binary_hint.rs
@@ -1,6 +1,3 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-
 use expander_compiler::frontend::*;
 
 declare_circuit!(Circuit {
@@ -60,36 +57,4 @@ fn test_300() {
         let output = compile_result.layered_circuit.run(&witness);
         assert_eq!(output, vec![i < 256]);
     }
-}
-
-#[test]
-fn test_300_closure() {
-    let mut hint_registry = HintRegistry::<M31>::new();
-    let call_count = Rc::new(RefCell::new(0));
-    let call_count_clone = call_count.clone();
-    hint_registry.register(
-        "your_hint_namespace.sub_namespace.tobinary",
-        move |x: &[M31], y: &mut [M31]| -> Result<(), Error> {
-            *call_count_clone.borrow_mut() += 1;
-            let t = x[0].to_u256();
-            for (i, k) in y.iter_mut().enumerate() {
-                *k = M31::from_u256(t >> i as u32 & 1);
-            }
-            Ok(())
-        },
-    );
-
-    let compile_result = compile(&Circuit::default(), CompileOptions::default()).unwrap();
-    for i in 0..300 {
-        let assignment = Circuit::<M31> {
-            input: M31::from(i as u32),
-        };
-        let witness = compile_result
-            .witness_solver
-            .solve_witness_with_hints(&assignment, &mut hint_registry)
-            .unwrap();
-        let output = compile_result.layered_circuit.run(&witness);
-        assert_eq!(output, vec![i < 256]);
-    }
-    assert_eq!(*call_count.borrow(), 300);
 }
