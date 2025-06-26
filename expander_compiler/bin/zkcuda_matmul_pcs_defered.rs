aa92f5ee492391b9e9d55cae6a0b66297b846da8
Zhiyong Fang <zhiyong.fang.1997@gmail.com>
2025-06-25 20:20:04 -0500

    some more tests

--- expander_compiler/bin/zkcuda_matmul_pcs_defered.rs
+++ expander_compiler/bin/zkcuda_matmul_pcs_defered.rs
@@ -1,6 +1,9 @@
 #![allow(unused)]
 mod zkcuda_matmul;
-use expander_compiler::{frontend::BN254Config, zkcuda::proving_system::ExpanderPCSDefered};
+use expander_compiler::{
+    frontend::BN254Config,
+    zkcuda::proving_system::{expander_pcs_defered::BN254ConfigSha2UniKZG, ExpanderPCSDefered},
+};
 use gkr::BN254ConfigSha2Hyrax;
 use zkcuda_matmul::zkcuda_matmul;
 
@@ -8,4 +11,8 @@ fn main() {
     zkcuda_matmul::<BN254Config, ExpanderPCSDefered<BN254ConfigSha2Hyrax>, 4>();
     zkcuda_matmul::<BN254Config, ExpanderPCSDefered<BN254ConfigSha2Hyrax>, 8>();
     zkcuda_matmul::<BN254Config, ExpanderPCSDefered<BN254ConfigSha2Hyrax>, 16>();
+
+    zkcuda_matmul::<BN254Config, ExpanderPCSDefered<BN254ConfigSha2UniKZG>, 4>();
+    zkcuda_matmul::<BN254Config, ExpanderPCSDefered<BN254ConfigSha2UniKZG>, 8>();
+    zkcuda_matmul::<BN254Config, ExpanderPCSDefered<BN254ConfigSha2UniKZG>, 16>();
 }
